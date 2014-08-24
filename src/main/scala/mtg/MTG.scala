a978b34c929bfb771497934c537e591cb9e4fbcc
rafanoronha <rafanoronha@gmail.com>
2014-08-24 12:29:23 -0300

    permanent props plus additional spell behavior

--- src/main/scala/mtg/MTG.scala
+++ src/main/scala/mtg/MTG.scala
@@ -1,15 +1,25 @@
 case class Player
 
-abstract class Permanent
+trait Card
+
+trait Spell extends SpellOrAbility {
+    val card: Card
+}
+
+trait Permanent {
+    val properties: Seq[PermanentProperty]    
+}
 
 trait PermanentProperty
 
 trait CantAttack extends PermanentProperty
 trait CantBlock extends PermanentProperty
 
-case class Creature(power: Int, toughness: Int) extends Permanent {
+case class AdditionalPower(value: Int) extends PermanentProperty
+
+case class Creature(power: Int, toughness: Int, properties: Seq[PermanentProperty]) extends Permanent {
     def mayAttack = true
-    def mayBlock = true    
+    def mayBlock = true
 }
 
 case class Battlefield(val creatures: Seq[Creature])
@@ -30,6 +40,29 @@ sealed trait Effect {
     def affect(table: Table) : Table
 }
 
+case class DiscardsCardEffect(card: Card) extends Effect {
+    // FIXME put card on graveyard
+    def affect(table: Table) = table
+}
+
+case class SequenceOfEffects(effects: Seq[Effect]) extends Effect {
+    def affect(table: Table) = loop(table, effects)
+    def loop(table: Table, effects: Seq[Effect]): Table =  effects match {
+        case e :: es => loop(e.affect(table), es)
+        case Nil => table
+    }
+}
+
+trait AddsPermanentPropertyEffect[P, PP] extends Effect {
+    val permanent: P
+    val permanentProperty: PP
+    // FIXME put property on permanent
+    def affect(table: Table) = table
+}
+
+case class AddsAdditionalPowerEffect(permanent: Creature, permanentProperty: AdditionalPower)
+    extends AddsPermanentPropertyEffect[Creature, AdditionalPower]
+
 case class EntersTheBattlefield(creature: Creature) extends Effect {
     // FIXME put creature on battlefield
     def affect(table: Table) = table
@@ -44,7 +77,11 @@ sealed trait SpellOrAbility {
     def resolve: Effect
 }
 
-abstract class SingleTargetSpell[T](target: T, resolveFun: (T) => Effect) extends SpellOrAbility {
+trait InstantOrSorcery extends Spell {
+    abstract override def resolve: Effect = SequenceOfEffects(DiscardsCardEffect(card) :: super.resolve :: Nil)
+}
+
+abstract class SingleTargetSpell[T](card: Card, target: T, resolveFun: (T) => Effect) extends Spell {
     def resolve = resolveFun(target)
 }
 
@@ -52,4 +89,5 @@ case class CreatureSpell(creature: Creature) extends SpellOrAbility {
     def resolve = new EntersTheBattlefield(creature)
 }
 
-case class CounterSpell(target: SpellOrAbility) extends SingleTargetSpell[SpellOrAbility](target, new CounteredSpell(_))
+case class CounterSpell(card: Card, target: SpellOrAbility)
+    extends SingleTargetSpell[SpellOrAbility](card, target, new CounteredSpell(_)) with InstantOrSorcery
